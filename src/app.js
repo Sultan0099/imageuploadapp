0694666fa7b4c18052ae4d1a6cc44ad1f8bad17b
syedsultan <syedsultan0099@gmail.com>
2019-08-04 19:24:31 +0500

    database permission

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes/index");
-const multer = require("multer");
 // databse config
 mongoose
   .connect(
